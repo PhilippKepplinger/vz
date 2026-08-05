bc14093f256784be85ae1153cb2d6605ce49e30c
Philipp Kepplinger <philipp.kepplinger@outlook.at>
2026-08-06 00:05:02 +0200

    V0.1.0
    * More refined ffmpeg pipeline
    
    * Renderer init
    
    * First working draft
    
    * Some refinements
    
    * Add build.sh

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,10 @@
 const std = @import("std");
 const Io = std.Io;
-const ffmpeg = @import("ffmpeg.zig");
+const ffmpeg = @import("ffmpeg.zig").ffmpeg;
 const Demuxer = @import("demuxer.zig").Demuxer;
 const Decoder = @import("decoder.zig").Decoder;
 const FrameReader = @import("reader.zig").FrameReader;
+const FrameScaler = @import("scaler.zig").FrameScaler;
 const FrameRenderer = @import("renderer.zig").FrameRenderer;
 
 pub fn main(init: std.process.Init) !void {
@@ -19,13 +20,27 @@ pub fn main(init: std.process.Init) !void {
     const file_path = args[1][0.. :0];
     std.log.debug("open file: {s}", .{file_path});
 
+    var renderBuffer: [512]u8 = undefined;
     var demuxer = try Demuxer.init(file_path);
     var decoder = try Decoder.init(&demuxer);
     var reader = try FrameReader.init(&demuxer, &decoder);
-    var renderer = FrameRenderer.init();
+    var renderer = FrameRenderer.init(init.io, renderBuffer[0..]);
+    var scaler = try FrameScaler.init(
+        decoder.videoInfo.codec,
+        renderer.width(),
+        renderer.height() * 2, // use twice the height because terminal cells are twice as higher than wide
+        ffmpeg.AV_PIX_FMT_GRAY8,
+    );
+
+    try renderer.clear();
 
     while (reader.next()) |frame| {
-        try renderer.render(frame);
+        if (frame.width > 0) { // video frame
+            const scaledFrame = try scaler.scale(frame);
+            try renderer.render(scaledFrame);
+        } else {
+            // TODO audio frame
+        }
     } else |err| {
         if (err == error.EOF) {
             std.log.info("EOF reached", .{});
--- src/renderer.zig
+++ src/renderer.zig
@@ -1,14 +1,107 @@
 const std = @import("std");
 const ffmpeg = @import("ffmpeg.zig").ffmpeg;
+const scaler = @import("scaler.zig");
 
 pub const FrameRenderer = struct {
-    pub fn init() FrameRenderer {
-        return .{};
+    ws: std.posix.winsize = undefined,
+    io: std.Io,
+    writer: std.Io.File.Writer,
+
+    pub fn init(io: std.Io, buffer: []u8) FrameRenderer {
+        var ws: std.posix.winsize = undefined;
+        _ = std.os.linux.ioctl(std.posix.STDOUT_FILENO, std.os.linux.T.IOCGWINSZ, @intFromPtr(&ws));
+
+        std.log.debug("window size: {d}x{d}", .{ ws.col, ws.row });
+
+        return .{
+            .io = io,
+            .ws = ws,
+            .writer = std.Io.File.stdout().writer(io, buffer),
+        };
+    }
+
+    pub fn width(self: *@This()) u16 {
+        return self.ws.col;
+    }
+
+    pub fn height(self: *@This()) u16 {
+        return self.ws.row;
     }
 
     pub fn render(self: *@This(), frame: *ffmpeg.AVFrame) !void {
-        // TODO
-        _ = self;
-        _ = frame;
+        try self.toHome();
+
+        const pixels = frame.data[0];
+        const linesize: usize = @intCast(frame.linesize[0]);
+
+        const terminalHeight: usize = @as(u64, @intCast(frame.height)) / 2;
+        for (0..terminalHeight) |row| {
+            for (0..@intCast(frame.width)) |col| {
+                if (col >= self.width()) {
+                    continue;
+                }
+                const top = pixels[2 * row * linesize + col];
+                const bottom = pixels[(2 * row + 1) * linesize + col];
+                const char = getChar(top, bottom);
+
+                try self.writer.interface.print("{u}", .{char});
+            }
+
+            try self.writer.interface.writeByte('\n');
+        }
+    }
+
+    fn getChar(top: u8, bottom: u8) u21 {
+        const topLevel = brightnessLevel(top);
+        const bottomLevel = brightnessLevel(bottom);
+
+        if (topLevel == bottomLevel) {
+            return switch (topLevel) {
+                0 => ' ',
+                1 => '░',
+                2 => '▒',
+                else => '█',
+            };
+        }
+
+        if (topLevel > bottomLevel) {
+            if (topLevel == 3 and bottomLevel >= 2) {
+                return '▓';
+            } else {
+                return '▀';
+            }
+        }
+
+        if (bottomLevel == 3 and topLevel >= 2) {
+            return '▓';
+        }
+
+        return '▄';
+    }
+
+    fn brightnessLevel(value: u8) u8 {
+        return switch (value) {
+            0...63 => 0,
+            64...127 => 1,
+            128...191 => 2,
+            else => 3,
+        };
+    }
+
+    pub fn clear(self: *@This()) !void {
+        try self.writer.interface.writeAll("\x1b[2J");
+    }
+
+    pub fn writeCharAt(self: *@This(), char: u8, row: usize, col: usize) !void {
+        try self.moveTo(row, col);
+        try self.writer.interface.writeByte(char);
+    }
+
+    fn toHome(self: *@This()) !void {
+        try self.writer.interface.writeAll("\x1b[H");
+    }
+
+    fn moveTo(self: *@This(), row: usize, col: usize) !void {
+        try self.writer.interface.print("\x1b[{};{}H", .{ row + 1, col + 1 });
     }
 };
