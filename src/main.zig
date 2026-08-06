bb5f750929306a3504dd0c387fbffefc98b85055
Philipp Kepplinger <philipp.kepplinger@outlook.at>
2026-08-06 19:13:18 +0200

    Introduce render modes

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,8 @@ const Demuxer = @import("demuxer.zig").Demuxer;
 const Decoder = @import("decoder.zig").Decoder;
 const FrameReader = @import("reader.zig").FrameReader;
 const FrameScaler = @import("scaler.zig").FrameScaler;
-const FrameRenderer = @import("renderer.zig").FrameRenderer;
+const renderer = @import("renderer.zig");
+const FrameRenderer = renderer.FrameRenderer;
 
 pub fn main(init: std.process.Init) !void {
     const arena: std.mem.Allocator = init.arena.allocator();
@@ -17,27 +18,29 @@ pub fn main(init: std.process.Init) !void {
     }
 
     // zero terminate file path
-    const file_path = args[1][0.. :0];
+    const file_path = args[args.len - 1][0.. :0];
     std.log.debug("open file: {s}", .{file_path});
 
     var renderBuffer: [512]u8 = undefined;
+    const renderMode = getMode(args);
+
     var demuxer = try Demuxer.init(file_path);
     var decoder = try Decoder.init(&demuxer);
     var reader = try FrameReader.init(&demuxer, &decoder);
-    var renderer = FrameRenderer.init(init.io, renderBuffer[0..]);
+    var frameRenderer = FrameRenderer.init(init.io, renderMode, renderBuffer[0..]);
     var scaler = try FrameScaler.init(
         decoder.videoInfo.codec,
-        renderer.width(),
-        renderer.height() * 2, // use twice the height because terminal cells are twice as higher than wide
+        frameRenderer.width(),
+        frameRenderer.height() * 2, // use twice the height because terminal cells are twice as higher than wide
         ffmpeg.AV_PIX_FMT_GRAY8,
     );
 
-    try renderer.clear();
+    try frameRenderer.clear();
 
     while (reader.next()) |frame| {
         if (frame.width > 0) { // video frame
             const scaledFrame = try scaler.scale(frame);
-            try renderer.render(scaledFrame);
+            try frameRenderer.render(scaledFrame);
         } else {
             // TODO audio frame
         }
@@ -48,4 +51,16 @@ pub fn main(init: std.process.Init) !void {
     }
 
     demuxer.close();
+}
+
+fn getMode(args: []const [:0]const u8) renderer.RenderMode {
+    for (args, 0..) |arg, i| {
+        if (std.mem.eql(u8, arg, "-m")) {
+            if (std.mem.eql(u8, args[i + 1], "0")) return renderer.RenderMode.ASCII;
+            if (std.mem.eql(u8, args[i + 1], "1")) return renderer.RenderMode.GRAY;
+            if (std.mem.eql(u8, args[i + 1], "2")) return renderer.RenderMode.COMIC;
+        }
+    }
+
+    return renderer.RenderMode.ASCII;
 }
--- src/renderer.zig
+++ src/renderer.zig
@@ -2,12 +2,19 @@ const std = @import("std");
 const ffmpeg = @import("ffmpeg.zig").ffmpeg;
 const scaler = @import("scaler.zig");
 
+pub const RenderMode = enum {
+    GRAY,
+    ASCII,
+    COMIC,
+};
+
 pub const FrameRenderer = struct {
+    mode: RenderMode,
     ws: std.posix.winsize = undefined,
     io: std.Io,
     writer: std.Io.File.Writer,
 
-    pub fn init(io: std.Io, buffer: []u8) FrameRenderer {
+    pub fn init(io: std.Io, mode: RenderMode, buffer: []u8) FrameRenderer {
         var ws: std.posix.winsize = undefined;
         _ = std.os.linux.ioctl(std.posix.STDOUT_FILENO, std.os.linux.T.IOCGWINSZ, @intFromPtr(&ws));
 
@@ -17,6 +24,7 @@ pub const FrameRenderer = struct {
             .io = io,
             .ws = ws,
             .writer = std.Io.File.stdout().writer(io, buffer),
+            .mode = mode,
         };
     }
 
@@ -42,9 +50,12 @@ pub const FrameRenderer = struct {
                 }
                 const top = pixels[2 * row * linesize + col];
                 const bottom = pixels[(2 * row + 1) * linesize + col];
-                const char = getChar(top, bottom);
 
-                try self.writer.interface.print("{u}", .{char});
+                switch (self.mode) {
+                    RenderMode.GRAY => try self.renderGray(top, bottom),
+                    RenderMode.COMIC => try self.renderCOMIC(top, bottom),
+                    RenderMode.ASCII => try self.renderASCII(top, bottom),
+                }
             }
 
             if (row < self.height() - 1) {
@@ -53,7 +64,67 @@ pub const FrameRenderer = struct {
         }
     }
 
-    fn getChar(top: u8, bottom: u8) u21 {
+    fn renderGray(self: *@This(), top: u8, bottom: u8) !void {
+        try self.writer.interface.print(
+            "\x1b[38;2;{};{};{}m\x1b[48;2;{};{};{}m{u}",
+            .{ top, top, top, bottom, bottom, bottom, '▀' },
+        );
+    }
+
+    fn renderASCII(self: *@This(), top: u8, bottom: u8) !void {
+        const level = getAvgLevel(top, bottom);
+        const brightness = getReducedBrightness(level);
+        const char = getChar(top, bottom, brightness);
+
+        try self.writer.interface.print(
+            "\x1b[38;2;{};{};{}m{u}",
+            .{ brightness, brightness, brightness, char },
+        );
+    }
+
+    fn getAvgLevel(top: u8, bottom: u8) u16 {
+        return (@as(u16, top) + @as(u16, bottom)) / 2;
+    }
+
+    fn getReducedBrightness(level: u16) u8 {
+        return switch (level) {
+            0...31 => 32,
+            32...63 => 64,
+            64...95 => 96,
+            96...127 => 128,
+            128...159 => 160,
+            160...191 => 192,
+            192...223 => 224,
+            else => 255,
+        };
+    }
+
+    fn getChar(top: u8, bottom: u8, level: u8) u21 {
+        const i = @divTrunc(level, 31) - 1;
+        const veryTopHeavy = top > bottom and top - bottom > 80;
+        const topHeavy = top > bottom and top - bottom > 30;
+        const balanced = (top > bottom and top - bottom <= 30) or (top < bottom and bottom - top <= 30);
+        const veryBottomHeavy = bottom > top and bottom - top > 80;
+
+        if (veryTopHeavy) {
+            return "`'\"^~V/TM"[i];
+        } else if (topHeavy) {
+            return ".-~=itfx"[i];
+        } else if (balanced) {
+            return "oxsz*#%@"[i];
+        } else if (veryBottomHeavy) {
+            return "gumwh&W8"[i];
+        } else {
+            return "..,_jypq"[i];
+        }
+    }
+
+    fn renderCOMIC(self: *@This(), top: u8, bottom: u8) !void {
+        const char = getBlockChar(top, bottom);
+        try self.writer.interface.print("{u}", .{char});
+    }
+
+    fn getBlockChar(top: u8, bottom: u8) u21 {
         const topLevel = brightnessLevel(top);
         const bottomLevel = brightnessLevel(bottom);
 
